5d1e0a1d6ead3b957a32874793fc07980ccc64b7
Mariam Fahmy <mariamfahmy66@gmail.com>
2022-06-04 23:22:01 +0200

    forwardcheckingsolver

--- lib/solver/forwardcheckingsolver.cpp
+++ lib/solver/forwardcheckingsolver.cpp
@@ -1,6 +1,32 @@
 #include "forwardcheckingsolver.h"
 
-ForwardCheckingSolver::ForwardCheckingSolver() : KenKenSolver()
-{
+ForwardCheckingSolver::ForwardCheckingSolver() : KenKenSolver() {}
 
+vector<uint8_t> ForwardCheckingSolver::get_domian(const Cell &cell) {
+  set<uint8_t> domain = m_board.fc_domain(cell);
+  return vector<uint8_t>(domain.begin(), domain.end());
+}
+
+bool ForwardCheckingSolver::check_domains(const optional<Cell> &cell) {
+  if (!cell)
+    return true;
+  vector<Cell> cells;
+  for (uint8_t i = cell->first + 1; i < m_board.size(); i++) {
+    cells.push_back({i, cell->second});
+  }
+
+  for (uint8_t i = cell->second + 1; i < m_board.size(); i++) {
+    cells.push_back({cell->first, i});
+  }
+
+  for (Cell related_cell : m_board.get_constraint(*cell).cells()) {
+    if (related_cell != *cell)
+      cells.push_back(related_cell);
+  }
+
+  for (Cell related_cell : cells) {
+    if (m_board.fc_domain(related_cell).size() == 0)
+      return false;
+  }
+  return true;
 }
--- lib/solver/forwardcheckingsolver.h
+++ lib/solver/forwardcheckingsolver.h
@@ -3,42 +3,15 @@
 
 #include "kenkensolver.h"
 
-class ForwardCheckingSolver : public KenKenSolver
-{
+class ForwardCheckingSolver : public KenKenSolver {
 public:
   ForwardCheckingSolver();
   ForwardCheckingSolver(KenKenBoard board) : KenKenSolver(board) {}
   virtual ~ForwardCheckingSolver() {}
 
 protected:
-  vector<uint8_t> get_domian(const Cell& cell) override {
-    set<uint8_t> domain = m_board.fc_domain(cell);
-    return vector<uint8_t>(domain.begin(), domain.end());
-  }
-
-  bool check_domains(const optional<Cell>& cell) override {
-    if(!cell)
-      return true;
-    vector<Cell> cells;
-    for(uint8_t i = cell->first + 1; i < m_board.size(); i++) {
-      cells.push_back({i, cell->second});
-    }
-
-    for(uint8_t i = cell->second + 1; i < m_board.size(); i++) {
-      cells.push_back({cell->first, i});
-    }
-
-    for(Cell related_cell : m_board.get_constraint(*cell).cells()) {
-      if(related_cell != *cell)
-        cells.push_back(related_cell);
-    }
-
-    for(Cell related_cell : cells) {
-      if(m_board.fc_domain(related_cell).size() == 0)
-        return false;
-    }
-    return true;
-  }
+  vector<uint8_t> get_domian(const Cell &cell) override;
+  bool check_domains(const optional<Cell> &cell) override;
 };
 
 #endif // FORWARDCHECKINGSOLVER_H
